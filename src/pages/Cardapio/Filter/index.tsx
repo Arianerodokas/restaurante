f92957bdc79cdf8543606a1f475f6eda7b26016c
Arianerodokas <barbosaarianerodokas@gmail.com>
2023-10-05 11:44:43 -0300

    button do filter

--- src/pages/Cardapio/Filter/index.tsx
+++ src/pages/Cardapio/Filter/index.tsx
@@ -1,27 +1,29 @@
+import styles from "./Filter.module.scss";
+import filter from "./filter.json";
 
-import styles from './Filter.module.scss';
-import filter from './filter.json';
+type IOpcao = typeof filter[0];
 
-type Iopcao = typeof filter[0];
+interface Props {
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
 
-/*interface IOpcao {
-    id: number;
-    label: string;
-}*/
-
-export default function Filter (){
-    return(
-
-<div className={styles.filtro}>
-    {filter.map((opcao) => (
-        <button className={styles.filtro__filtro} key={opcao.id}>
-            {opcao.label}
+export default function Filter({ filtro, setFiltro }: Props) {
+  function selecionarFilter(opcao: IOpcao) {
+    return setFiltro(opcao.id);
+  }
+  return (
+    <div className={styles.filtro}>
+      {filter.map((opcao) => (
+        <button className={`
+        ${styles.filtro__filtro}
+        ${filtro === opcao.id ? styles["filtro__filtro--ativo"]: ""}
+        `} 
+        key={opcao.id}
+        onClick={()=> selecionarFilter(opcao)}>
+          {opcao.label}
         </button>
-    
-    ))}
-</div>
-
-    )
-
-
+      ))}
+    </div>
+  );
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,33 +1,28 @@
-import styles from './Cardapio.module.scss';
-import logo from 'assets/logo.svg'
-import Filtro from './Filtro';
-import { useState } from 'react';
-import Filter from './Filter/index';
+import styles from "./Cardapio.module.scss";
+import logo from "assets/logo.svg";
+import Filtro from "./Filtro";
+import { useState } from "react";
+import Filter from "./Filter/index";
 
 export default function Cardapio() {
-    const [busca, setBusca] = useState("");
-    return (
-
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  return (
     <main>
-        <nav className={styles.menu}>
-            <img src={logo} alt="logo" />
-        </nav>
-        <header className={styles.header}>
-            <div className={styles.header__text}>
-            Pancia piena, felicità piena!
-            </div>
-        </header>
+      <nav className={styles.menu}>
+        <img src={logo} alt="logo" />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>Pancia piena, felicità piena!</div>
+      </header>
 
-        <section className={styles.cardapio}>
-            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-            <Filtro busca={busca} setBusca={setBusca}/>
-            <div className={styles.cardapio__filter}>
-         <Filter/>   
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Filtro busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filter}>
+            <Filter filtro={filtro} setFiltro={setFiltro} />
         </div>
-        </section>
-
-    </main>       
-
-    );
-
+      </section>
+    </main>
+  );
 }

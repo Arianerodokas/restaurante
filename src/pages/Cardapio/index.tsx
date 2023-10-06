bb60e005e17e3917536b651ed1fbefcc67d2232a
Arianerodokas <barbosaarianerodokas@gmail.com>
2023-10-06 11:18:51 -0300

    06-10

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./Cardapio.module.scss";
-import logo from "assets/logo.svg";
+import logo from "assets/logo.png";
 import Filtro from "./Filtro";
 import { useState } from "react";
 import Filter from "./Filter/index";
@@ -9,15 +9,15 @@ export default function Cardapio() {
   const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
-      <nav className={styles.menu}>
+      {/* <nav className={styles.menu__img}>
         <img src={logo} alt="logo" />
-      </nav>
+      </nav> */}
       <header className={styles.header}>
-        <div className={styles.header__text}>Pancia piena, felicità piena!</div>
+        <div className={styles.header__text}>Aproveite o frescor da Vida</div>
       </header>
 
       <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <h3 className={styles.cardapio__titulo}>Tipos de sucos</h3>
         <Filtro busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filter}>
             <Filter filtro={filtro} setFiltro={setFiltro} />
